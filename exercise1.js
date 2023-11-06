2fb87de304c85193ca43c5703301bd4566941e8e
Syamim <b022210085@student.utem.edu.my>
2023-11-06 23:17:19 +0800

    trytest

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+let data = {
+    "useraneme": "soo",
+    "password": "1234",
+    "age": 20
+}
+console.log(data)
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.use(express.json())
+
 app.get('/', (req, res) => {
- res.send('Hazim idola Syamim')
+ res.send('Hello World')
 })
+
+app.post('/login', (req, res) => {
+    if (req.body.username !== 'Apip') {
+        res.status(400).send('Invalid username')
+        return
+    }
+    if (req.body.password !== 'Apip123') {
+        res.status(401).send('Invalid password')
+        return
+    }
+    res.send('login successfully')
+})
+
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
